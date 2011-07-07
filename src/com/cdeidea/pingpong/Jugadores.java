73c51f570de51dbcc62dcc2b3a9bffee21f60502
manuel <manuel.pol@gmail.com>
2011-07-07 12:26:53 +0200

    Añadidos varios Activitys y menú inferior

---
+++ src/com/cdeidea/pingpong/Jugadores.java
@@ -0,0 +1,11 @@
+package com.cdeidea.pingpong;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Jugadores extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jugadores);
+    }
+}
--- src/com/cdeidea/pingpong/PingPong.java
+++ src/com/cdeidea/pingpong/PingPong.java
@@ -1,7 +1,13 @@
 package com.cdeidea.pingpong;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class PingPong extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +15,30 @@ public class PingPong extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
     }
+    
+	public boolean onCreateOptionsMenu(Menu menu){
+		MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item){
+		  	switch(item.getItemId()){
+			   case R.id.MenuNombre:
+				   Intent intent = new Intent(this, Jugadores.class);
+				   startActivity(intent);
+				   return true;
+			   case R.id.MenuAcerca:
+				   this.showMsg("Juego desarrollado por manuel.pol@gmail.com");
+			}
+		   return true;
+	}
+	
+	private void showMsg(String message) {
+		Toast msg = Toast.makeText(PingPong.this, message, Toast.LENGTH_LONG);
+		msg.setGravity(Gravity.CENTER, msg.getXOffset() / 2,msg.getYOffset() / 2);
+		msg.show();
+	}
 }
